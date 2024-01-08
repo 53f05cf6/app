eb5f7e4425cd9925debaa4a8fba8e11a0fae98ee
許煜廷 <me@hsuyuting.com>
2024-01-08 23:24:18 +0800

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module 53f05cf6
+
+go 1.22.1
+
+require github.com/mattn/go-sqlite3 v1.14.22
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "ti.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		rows, err := db.Query("SELECT id, title, content, created_at FROM news ORDER BY id DESC")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rows.Close()
+
+		var news []News
+		for rows.Next() {
+			var n News
+			err := rows.Scan(&n.ID, &n.Title, &n.Content, &n.CreatedAt)
+			if err != nil {
+				log.Fatal(err)
+			}
+			news = append(news, n)
+		}
+
+		tmpl, err := template.ParseFiles("index.html")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(news)
+
+		// print result
+		err = tmpl.Execute(w, news)
+		if err != nil {
+			log.Fatal(err)
+		}
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
