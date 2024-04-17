4bbf9f9b615ce7fcd5b3deacd0199384b0fd5b46
許煜廷 <you@hsuyuting.com>
2024-04-17 15:51:42 +0800

    Add logging

--- main.go
+++ main.go
@@ -15,6 +15,12 @@ import (
 )
 
 func main() {
+	logFile, err := os.OpenFile("./log", os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.SetOutput(logFile)
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
@@ -27,88 +33,81 @@ func main() {
 
 	cwaToken := os.Getenv("CWA_TOKEN")
 	if cwaToken == "" {
-		log.Fatal("OpenAI token missing")
+		log.Fatal("CWA token missing")
 	}
 
-	http.HandleFunc("/{$}", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodGet:
-			tmpl, err := template.ParseFiles("index.html")
-			if err != nil {
-				log.Fatal(err)
-			}
-			err = tmpl.Execute(w, nil)
-			if err != nil {
-				log.Fatal(err)
-			}
+	tmpl, err := template.ParseFiles("index.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	http.HandleFunc("GET /{$}", func(w http.ResponseWriter, r *http.Request) {
+		err = tmpl.Execute(w, nil)
+		if err != nil {
+			log.Panic(err)
 		}
 	})
 
-	http.HandleFunc("/weather/{$}", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("GET /weather/{$}", func(w http.ResponseWriter, r *http.Request) {
+		cwaRes, err := http.Get("https://opendata.cwa.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=" + cwaToken)
+		if err != nil {
+			log.Panic(err)
+		}
+		defer cwaRes.Body.Close()
+		body, err := io.ReadAll(cwaRes.Body)
+
+		cwa := Cwa{}
+		json.Unmarshal(body, &cwa)
+
+		var station Station
+		for _, s := range cwa.Records.Stations {
+			if s.GeoInfo.TownName == "中正區" {
+				station = s
+			}
+		}
+
+		bytes, err := json.Marshal(station)
+		if err != nil {
+			log.Panic(err)
+		}
+
+		msgs := []openai.ChatCompletionMessage{
+			{
+				Role:    openai.ChatMessageRoleSystem,
+				Content: "你是一個台灣天氣app助理，你需要依照'當下時間'及'天氣數據'給出合適的穿衣建議;當下時間:" + time.Now().String() + ";天氣數據:" + string(bytes) + ";使用台灣正體中文;第一段為條列天氣數據第二段為穿衣建議及理由: <ul><li>中正區...</li><li>...</li><li>...</li></ul><p>...</p>，但是如果用戶提供偏好的介面需要因此做出用戶指定的回答。如果用戶輸入並非與功能相關則拒絕回答。",
+			},
+		}
+
 		queries := r.URL.Query()
-		switch r.Method {
-		case http.MethodGet:
-			client := openai.NewClient(openAiToken)
-			t := time.Now().String()
-			weather, err := http.Get("https://opendata.cwa.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=" + cwaToken)
-			if err != nil {
-				log.Fatal("%v", err)
-			}
-			defer weather.Body.Close()
-			body, err := io.ReadAll(weather.Body)
-			cwa := Cwa{}
-			json.Unmarshal(body, &cwa)
+		customMsg := queries.Get("custom")
+		if customMsg != "" {
+			msgs = append(msgs, openai.ChatCompletionMessage{
+				Role:    openai.ChatMessageRoleUser,
+				Content: customMsg,
+			})
 
-			var station Station
-
-			for _, s := range cwa.Records.Stations {
-				if s.GeoInfo.TownName == "中正區" {
-					station = s
-				}
-			}
-
-			bytes, err := json.Marshal(station)
-			if err != nil {
-				log.Fatal("%v", err)
-			}
-
-			msgs := []openai.ChatCompletionMessage{
-				{
-					Role:    openai.ChatMessageRoleSystem,
-					Content: "你是一個台灣天氣app助理，你需要依照'當下時間'及'天氣數據'給出合適的穿衣建議;當下時間:" + t + ";天氣數據:" + string(bytes) + ";使用台灣正體中文;第一段為條列天氣數據第二段為穿衣建議及理由: <ul><li>中正區...</li><li>...</li><li>...</li></ul><p>...</p>，但是如果用戶提供偏好的介面需要因此做出用戶指定的回答。如果用戶輸入並非與功能相關則拒絕回答。",
-				},
-			}
-
-			customMsg := queries.Get("custom")
-			if customMsg != "" {
-				msgs = append(msgs, openai.ChatCompletionMessage{
-					Role:    openai.ChatMessageRoleUser,
-					Content: customMsg,
-				})
-
-			}
-
-			resp, err := client.CreateChatCompletion(
-				context.Background(),
-				openai.ChatCompletionRequest{
-					Model:    openai.GPT4Turbo,
-					Messages: msgs,
-				},
-			)
-
-			if err != nil {
-				fmt.Printf("ChatCompletion error: %v\n", err)
-				return
-			}
-
-			snippet := resp.Choices[0].Message.Content
-
-			w.Write([]byte(snippet))
 		}
+
+		client := openai.NewClient(openAiToken)
+		resp, err := client.CreateChatCompletion(
+			context.Background(),
+			openai.ChatCompletionRequest{
+				Model:    openai.GPT4Turbo,
+				Messages: msgs,
+			},
+		)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		snippet := []byte(resp.Choices[0].Message.Content)
+		w.Write(snippet)
 
 	})
 
-	http.HandleFunc("/news/{$}", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("GET /news/{$}", func(w http.ResponseWriter, r *http.Request) {
 		queries := r.URL.Query()
 		switch r.Method {
 		case http.MethodGet:
